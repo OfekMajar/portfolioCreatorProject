da59feb090022311326fa10e36accdb408e5df4a
OfekMajar <ofek.majar@gmail.com>
2024-01-28 22:20:25 +0200

    added conditionally rendering

--- src/components/ResumeForm.jsx
+++ src/components/ResumeForm.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import WorkExperiences from "./WorkExperiences";
 function ResumeForm() {
-
-const [formData,setFormData]=useState({})
+  const [formData, setFormData] = useState({});
+  const [currentFormStage, setCurrentFormStage] = useState(1);
   const [workExpList, setWorkExpList] = useState([
     <div className="singleWorkExpContainer">
       <div className="lableAndInput">
@@ -42,53 +42,65 @@ const [formData,setFormData]=useState({})
       </div>,
     ]);
   };
-  const changeHandler=(e)=>{
-    formData[e.target.name]=e.target.value
-    setFormData({...formData})
+  const changeHandler = (e) => {
+    formData[e.target.name] = e.target.value;
+    setFormData({ ...formData });
+  };
+  const nextStage=()=>{
+
+  }
+  const prevStage=()=>{
+    
   }
   return (
     <form>
-      <section id="contactInfoSection">
-        <h2>Contact Info:</h2>
-        <div>
-          <div className="lableAndInput">
-            <label htmlFor="">First name</label>
-            <input type="text" name="firstName" />
+      {currentFormStage == 1 ? (
+        <section id="contactInfoSection">
+          <h2>Contact Info:</h2>
+          <div>
+            <div className="lableAndInput">
+              <label htmlFor="">First name</label>
+              <input type="text" name="firstName" />
+            </div>
+            <div className="lableAndInput">
+              <label htmlFor="">Last name</label>
+              <input type="text" name="lastName" />
+            </div>
+          </div>
+          <div>
+            <div className="lableAndInput">
+              <label htmlFor="">Email:</label>
+              <input name="email" type="email" />
+            </div>
+            <div className="lableAndInput">
+              <label htmlFor="">Phone number:</label>
+              <input name="phoneNumber" type="text" />
+            </div>
           </div>
           <div className="lableAndInput">
-            <label htmlFor="">Last name</label>
-            <input type="text" name="lastName" />
+            <label htmlFor="">About me:</label>
+            <textarea name="aboutMe" type="text" id="aboutMeInput" />
           </div>
-        </div>
-        <div>
-          <div className="lableAndInput">
-            <label htmlFor="">Email:</label>
-            <input name="email" type="email" />
-          </div>
-          <div className="lableAndInput">
-            <label htmlFor="">Phone number:</label>
-            <input name="phoneNumber" type="text" />
-          </div>
-        </div>
-        <div className="lableAndInput">
-          <label htmlFor="">About me:</label>
-          <textarea name="aboutMe" type="text" id="aboutMeInput" />
-        </div>
-      </section>
-      <div id="workExpContainer">
-        <h2>Work experience:</h2>
-        {workExpList.map((item) => {
-          return item;
-        })}
-        <button onClick={createMoreWorkExp}> add Another</button>
-      </div>
-      <div>
+        </section>
+      ) : null}
+      {currentFormStage == 2 ? (
+        <section id="workExpContainer">
+          <h2>Work experience:</h2>
+          {workExpList.map((item) => {
+            return item;
+          })}
+          <button onClick={createMoreWorkExp}> add Another</button>
+        </section>
+      ) : null}
+      {currentFormStage==3?<section id="educationContainer">
         <h2>Education</h2>
         <div className="lableAndInput">
           <label htmlFor=""></label>
           <input type="text" />
         </div>
-      </div>
+      </section>:null}
+          {}
+      
     </form>
   );
 }
