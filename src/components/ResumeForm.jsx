23865080564247bfbe54b4155ff6b98645e7c01d
OfekMajar <ofek.majar@gmail.com>
2024-01-29 10:14:44 +0200

    trying to add different change handler to wrokExp

--- src/components/ResumeForm.jsx
+++ src/components/ResumeForm.jsx
@@ -1,106 +1,153 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import WorkExperiences from "./WorkExperiences";
 function ResumeForm() {
   const [formData, setFormData] = useState({});
+  const [workExpData, setWorkExpData] = useState([{}]);
+
   const [currentFormStage, setCurrentFormStage] = useState(1);
+  const [userData, setUserData] = useState({});
+  const [amoutOfWorkExp, setAmoutOfWorkExp] = useState(1);
+
+  const changeContactInfoHandler = (e) => {
+    setWorkExpData([...workExpData,{}])
+  };
+
+  const changeWorkExpHandler=(e)=>{
+    
+  }
+
   const [workExpList, setWorkExpList] = useState([
-    <div className="singleWorkExpContainer">
-      <div className="lableAndInput">
-        <label htmlFor="">Company Name:</label>
-        <input type="text" />
-      </div>
-      <div className="lableAndInput">
-        <label htmlFor="">Role:</label>
-        <input type="text" />
-      </div>
-      <div className="lableAndInput">
-        <label htmlFor="">Time frame:</label>
-        <input type="text" />
-      </div>
-    </div>,
+    <WorkExperiences
+      changeHandler={changeWorkExpHandler}
+      amoutOfWorkExp={amoutOfWorkExp}
+    />,
   ]);
-  const [amoutOfWorkExp, setAmoutOfWorkExp] = useState(2);
+
   const createMoreWorkExp = (e) => {
     e.preventDefault();
     setAmoutOfWorkExp(amoutOfWorkExp + 1);
     console.log(amoutOfWorkExp);
     setWorkExpList([
       ...workExpList,
-      <div className="singleWorkExpContainer">
-        <div className="lableAndInput">
-          <label htmlFor="">Company Name:</label>
-          <input type="text" required name={`companyName${amoutOfWorkExp}`} />
-        </div>
-        <div className="lableAndInput">
-          <label htmlFor="">Role:</label>
-          <input type="text" required name={`Role${amoutOfWorkExp}`} />
-        </div>
-        <div className="lableAndInput">
-          <label htmlFor="">Time frame:</label>
-          <input type="text" required name={`timeFrame${amoutOfWorkExp}`} />
-        </div>
-      </div>,
+      <WorkExperiences
+        changeHandler={changeWorkExpHandler}
+        amoutOfWorkExp={amoutOfWorkExp + 1}
+      />,
     ]);
   };
-  const changeHandler = (e) => {
-    formData[e.target.name] = e.target.value;
-    setFormData({ ...formData });
-  };
-  const nextStage=()=>{
 
-  }
-  const prevStage=()=>{
-    
-  }
+  const nextStage = (e) => {
+    e.preventDefault();
+    userData[e.target.name] = { ...formData };
+    setUserData({ ...userData });
+    setCurrentFormStage(currentFormStage + 1);
+  };
+
+  const prevStage = (e) => {
+    e.preventDefault();
+    setCurrentFormStage(currentFormStage - 1);
+  };
+  const sumbitHandler = (e) => {
+    e.preventDefault();
+    console.log(userData);
+  };
   return (
-    <form>
+    <form onSubmit={sumbitHandler}>
       {currentFormStage == 1 ? (
         <section id="contactInfoSection">
           <h2>Contact Info:</h2>
           <div>
             <div className="lableAndInput">
               <label htmlFor="">First name</label>
-              <input type="text" name="firstName" />
+              <input
+                onChange={changeContactInfoHandler}
+                type="text"
+                name="firstName"
+                defaultValue={formData.firstName}
+              />
             </div>
             <div className="lableAndInput">
               <label htmlFor="">Last name</label>
-              <input type="text" name="lastName" />
+              <input
+                onChange={changeContactInfoHandler}
+                type="text"
+                name="lastName"
+                defaultValue={formData.lastName}
+              />
             </div>
           </div>
           <div>
             <div className="lableAndInput">
               <label htmlFor="">Email:</label>
-              <input name="email" type="email" />
+              <input
+                onChange={changeContactInfoHandler}
+                name="email"
+                type="email"
+                defaultValue={formData.email}
+              />
             </div>
             <div className="lableAndInput">
               <label htmlFor="">Phone number:</label>
-              <input name="phoneNumber" type="text" />
+              <input
+                onChange={changeContactInfoHandler}
+                name="phoneNumber"
+                type="text"
+                defaultValue={formData.phoneNumber}
+              />
             </div>
           </div>
           <div className="lableAndInput">
             <label htmlFor="">About me:</label>
-            <textarea name="aboutMe" type="text" id="aboutMeInput" />
+            <textarea
+              onChange={changeContactInfoHandler}
+              name="aboutMe"
+              type="text"
+              id="aboutMeInput"
+            />
           </div>
         </section>
-      ) : null}
-      {currentFormStage == 2 ? (
+      ) : currentFormStage == 2 ? (
         <section id="workExpContainer">
           <h2>Work experience:</h2>
           {workExpList.map((item) => {
+            // console.log();
             return item;
           })}
           <button onClick={createMoreWorkExp}> add Another</button>
         </section>
+      ) : currentFormStage == 3 ? (
+        <section id="educationContainer">
+          <h2>Education</h2>
+          <div className="lableAndInput">
+            <label htmlFor=""></label>
+            <input type="text" />
+          </div>
+        </section>
       ) : null}
-      {currentFormStage==3?<section id="educationContainer">
-        <h2>Education</h2>
-        <div className="lableAndInput">
-          <label htmlFor=""></label>
-          <input type="text" />
-        </div>
-      </section>:null}
-          {}
-      
+      <section id="nextAndPrevBtns">
+        {currentFormStage == 1 ? (
+          <div>
+            <button onClick={nextStage} name="userContactInfo">
+              {" "}
+              Next
+            </button>
+          </div>
+        ) : null}
+        {currentFormStage == 2 ? (
+          <div>
+            <button onClick={prevStage}> Prev</button>
+            <button onClick={nextStage} name="userWorkExp">
+              Next{" "}
+            </button>
+          </div>
+        ) : null}
+        {currentFormStage == 3 ? (
+          <div>
+            <button onClick={prevStage}> Prev</button>
+            <button>Sumbit </button>
+          </div>
+        ) : null}
+      </section>
     </form>
   );
 }
--- src/components/WorkExperiences.jsx
+++ src/components/WorkExperiences.jsx
@@ -1,19 +1,35 @@
 import React from "react";
 
-function WorkExperiences() {
+function WorkExperiences(props) {
+  const {changeHandler,amoutOfWorkExp} = props
   return (
-    <div>
+    <div className="singleWorkExpContainer">
       <div className="lableAndInput">
         <label htmlFor="">Company Name:</label>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={changeHandler}
+          required
+          name={`companyName${amoutOfWorkExp}`}
+        />
       </div>
       <div className="lableAndInput">
         <label htmlFor="">Role:</label>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={changeHandler}
+          required
+          name={`role${amoutOfWorkExp}`}
+        />
       </div>
       <div className="lableAndInput">
         <label htmlFor="">Time frame:</label>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={changeHandler}
+          required
+          name={`timeFrame${amoutOfWorkExp}`}
+        />
       </div>
     </div>
   );
