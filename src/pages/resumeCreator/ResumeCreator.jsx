a75e64a5c408571788d521724dfbcf3a30c05a49
OfekMajar <ofek.majar@gmail.com>
2024-03-03 09:41:00 +0200

    back to styling

--- src/pages/resumeCreator/ResumeCreator.jsx
+++ src/pages/resumeCreator/ResumeCreator.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import ResumeForm from "../../components/resumeFrom/ResumeForm";
 import Template2 from "../../components/templates/template2/Template2";
 import Template1 from "../../components/templates/template1/template1";
 import template1Img from "../../assets/Images/template1.png";
-import template2Img from"../../assets/Images/template2.png";
+import template2Img from "../../assets/Images/template2.png";
 import MiniTemplate from "../../components/templates/miniTamplate/MiniTemplate";
 import "./resumeCreator.css";
-
+import { UserContext } from "../../context/User";
+import { Link } from "react-router-dom";
 function ResumeCreator() {
+  const { user } = useContext(UserContext);
   const [isChoosingTemplate, setIsChoosingTemplate] = useState(true);
   const [selectedTemplate, setSelectedTemplate] = useState("1");
   const [resumeName, setResumeName] = useState("");
@@ -29,66 +31,79 @@ function ResumeCreator() {
     setIsChoosingTemplate(false);
   };
   return (
-    <div id="ResumeFormContainer">
-      {isChoosingTemplate ? (
-        <div id="templateChooserContainer">
-          <section id="templatesOptionsContainer">
-            <div className="templateChooserHeader">
-              <p >Choose your template</p>
-            </div>
-            <div id="templateOptions">
-              <div
-                className={`singleOptionBox ${
-                  selectedTemplate == "1" ? "selectedTemplate" : null
-                }`}>
-                <img
-                  src={template1Img}
-                  onClick={viewedTemplateSelector}
-                  name={"1"}
-                  alt="template1"
-                  className="templateOption"
+    <div className="ResumeCreatorPageContainer">
+      {user ? (
+        <div id="ResumeFormContainer">
+          {isChoosingTemplate ? (
+            <div id="templateChooserContainer">
+              <section id="templatesOptionsContainer">
+                <div className="templateChooserHeader">
+                  <p>Choose your template</p>
+                </div>
+                <div id="templateOptions">
+                  <div
+                    className={`singleOptionBox ${
+                      selectedTemplate == "1" ? "selectedTemplate" : null
+                    }`}>
+                    <img
+                      src={template1Img}
+                      onClick={viewedTemplateSelector}
+                      name={"1"}
+                      alt="template1"
+                      className="templateOption"
+                    />
+                  </div>
+                  <div
+                    className={`singleOptionBox ${
+                      selectedTemplate == "2" ? "selectedTemplate" : null
+                    }`}>
+                    <img
+                      src={template2Img}
+                      onClick={viewedTemplateSelector}
+                      name={"2"}
+                      alt="template2"
+                      className="templateOption"
+                    />
+                  </div>
+                </div>
+              </section>
+              <form
+                onSubmit={confirmTemplate}
+                className="confirmTemplateBtnContainer">
+                <input
+                  type="text"
+                  onChange={changeHandler}
+                  placeholder="Resume Name"
+                  required
                 />
-              </div>
-              <div
-                className={`singleOptionBox ${
-                  selectedTemplate == "2" ? "selectedTemplate" : null
-                }`}>
-                <img
-                  src={template2Img}
-                  onClick={viewedTemplateSelector}
-                  name={"2"}
-                  alt="template2"
-                  className="templateOption"
-                />
-              </div>
-
+                <button id="confirmTemplateBtn">Confirm</button>
+              </form>
+              <section id="templateViewer">
+                {selectedTemplate == "1" ? (
+                  <Template1 isPreview={true} />
+                ) : selectedTemplate == "2" ? (
+                  <Template2 isPreview={true}></Template2>
+                ) : selectedTemplate == 3 ? (
+                  3
+                ) : null}
+              </section>
             </div>
-          </section>
-          <div className="confirmTemplateBtnContainer">
-            <input
-              type="text"
-              onChange={changeHandler}
-              placeholder="Resume Name"
+          ) : (
+            <ResumeForm
+              resumeName={resumeName}
+              selectedTemplate={selectedTemplate}
             />
-            <button onClick={confirmTemplate} id="confirmTemplateBtn">
-              Confirm
-            </button>
-          </div>
-          <section id="templateViewer">
-            {selectedTemplate == "1" ? (
-              <Template1 isPreview={true} />
-            ) : selectedTemplate == "2" ? ( <Template2 isPreview={true}></Template2>) 
-             
-            : selectedTemplate == 3 ? (
-              3
-            ) : null}
-          </section>
+          )}
         </div>
       ) : (
-        <ResumeForm
-          resumeName={resumeName}
-          selectedTemplate={selectedTemplate}
-        />
+        <div className="myResumesErrorPageContainer">
+          <div className="userNotLoggedInErrorPage">
+            <p>Login to create resumes</p>
+            <button id="loggedInErrorBtn" className="authNavBtn">
+              <Link to={"/Authentication"}>Login</Link>
+            </button>
+          </div>
+        </div>
       )}
     </div>
   );
--- src/pages/singleResume/singleResume.jsx
+++ src/pages/singleResume/singleResume.jsx
@@ -1,66 +1,81 @@
-import React, { useContext, useEffect, useState } from 'react'
-import Template1Img from "../../assets/Images/template1.png"
-import Template1 from '../../components/templates/template1/template1'
-import { useParams } from 'react-router-dom'
-import { ResumesContext } from '../../context/Resumes'
-import { doc, getDoc } from 'firebase/firestore'
-import { db } from '../../config/firebase'
-import "./singleResume.css"
-import html2canvas from 'html2canvas'
-import jsPDF from 'jspdf'
+import React, { useContext, useEffect, useState } from "react";
+import Template1Img from "../../assets/Images/template1.png";
+import Template1 from "../../components/templates/template1/template1";
+import Template2 from "../../components/templates/template2/Template2";
+import { useParams } from "react-router-dom";
+import { ResumesContext } from "../../context/Resumes";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../config/firebase";
+import "./singleResume.css";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 function singleResume() {
-    const [singleResume,setSingleResume]=useState()
-   const [loader,setLoader]=useState(false)
-    const resumeId=useParams().ResumeId
-    
-    const downloadPDF = ()=>{
-        const capture = document.querySelector(`.actual-receipt`)
+  const [singleResume, setSingleResume] = useState();
+  const [loader, setLoader] = useState(false);
+  const resumeId = useParams().ResumeId;
 
-        setLoader(true)
-        html2canvas(capture).then((canvas)=>{
-            const imgData=canvas.toDataURL(`img/png`);
-            const doc=new jsPDF(`p`,`mm`,`a4`)
-            const componentWidth=doc.internal.pageSize.getWidth()
-            const componentHeight=doc.internal.pageSize.getHeight()
-            doc.addImage(imgData,`PNG`,0,0,componentWidth,componentHeight)
-            setLoader(false)
-            doc.save(`receipt.pdf`)
-        })
-    }
+  const downloadPDF = () => {
+    const capture = document.querySelector(`.actual-receipt`);
 
-    const getSingleResumeFromDb = async (resumeId)=>{
-        try {
-        const resumeDoc=  doc(db,"Resumes",resumeId)
-       const docSnapshot = await getDoc(resumeDoc)
-       if (docSnapshot.exists()) {
+    setLoader(true);
+    html2canvas(capture).then((canvas) => {
+      const imgData = canvas.toDataURL(`img/png`);
+      const doc = new jsPDF(`p`, `mm`, `a4`);
+      const componentWidth = doc.internal.pageSize.getWidth();
+      const componentHeight = doc.internal.pageSize.getHeight();
+      doc.addImage(imgData, `PNG`, 0, 0, componentWidth, componentHeight);
+      setLoader(false);
+      doc.save(`receipt.pdf`);
+    });
+  };
+
+  const getSingleResumeFromDb = async (resumeId) => {
+    try {
+      const resumeDoc = doc(db, "Resumes", resumeId);
+      const docSnapshot = await getDoc(resumeDoc);
+      if (docSnapshot.exists()) {
         const docData = docSnapshot.data();
         console.log(docData);
-        setSingleResume(docData)
-        
+        setSingleResume(docData);
       } else {
-        console.log('Document does not exist');
+        console.log("Document does not exist");
       }
-        } catch (error) {
-          console.error('Error fetching document:', error);
-        }
-      }
-    useEffect(()=>{
-        getSingleResumeFromDb(resumeId)
-    },[])
-   
-    
+    } catch (error) {
+      console.error("Error fetching document:", error);
+    }
+  };
+  useEffect(() => {
+    getSingleResumeFromDb(resumeId);
+  }, []);
+
   return (
-    <div id='singleResumeContainer'>   
-        {singleResume?  <Template1 isPreview={false} workExp={singleResume.userWorkExp} educations={singleResume.educations} contactInfo={singleResume.userContactInfo} />:null}
-     
-     <button onClick={downloadPDF}
-     disabled={!(loader===false)}>  {loader?(
-        <span>Downloading</span>
-     ):(<span>Donwload</span>)}
-      </button>
-   
+    <div id="singleResumePageContainer">
+      {singleResume ? (
+        <div id="singleResumeContainer">
+          {" "}
+          {singleResume.selectedTemplate == 1 ? (
+            <Template1
+              isPreview={false}
+              workExp={singleResume.userWorkExp}
+              educations={singleResume.educations}
+              contactInfo={singleResume.userContactInfo}
+            />
+          ) : singleResume.selectedTemplate == 2 ? (
+            <Template2
+              isPreview={false}
+              workExp={singleResume.userWorkExp}
+              educations={singleResume.educations}
+              contactInfo={singleResume.userContactInfo}
+            />
+          ) : null}
+          <button className="downloadBtn" onClick={downloadPDF} disabled={!(loader === false)}>
+            
+            {loader ? <span>Downloading</span> : <span>Donwload</span>}
+          </button>
+        </div>
+      ) : null}
     </div>
-  )
+  );
 }
 
-export default singleResume
+export default singleResume;
